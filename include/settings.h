047162365114cf90cb39f66ca59947a00cbf9486
dilush hashen <dilushhashen11@gmail.com>
2025-10-28 20:12:10 +0530

    #3 feat: can change cell state by clicking the cell

---
+++ include/settings.h
@@ -0,0 +1,15 @@
+#ifndef SETTING_H
+#define SETTING_H
+
+
+// COLORS
+#define BG_COLOR   0x0, 0x0, 0x0, 0xFF
+#define CELL_COLOR 240, 235, 84, 0xFF
+#define MESH_COLOR 79, 79, 79, 0xFF
+
+
+// SIZES
+#define CELL_SIZE 5 
+
+
+#endif
